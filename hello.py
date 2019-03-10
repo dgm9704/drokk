395ceda9bc58409f0394029babd1febb3debaa65
dgm9704 <dgm9704@hotmail.com>
2019-03-11 00:55:39 +0200

    formatting

--- hello.py
+++ hello.py
@@ -3,16 +3,26 @@
 
 import json
 from curses import wrapper
+import curses
 
 def main(stdscr):
+    HANDLE_COLOR = 1
+    NAME_COLOR = 2
+    curses.init_pair(HANDLE_COLOR,curses.COLOR_MAGENTA,curses.COLOR_BLACK)
+    curses.init_pair(NAME_COLOR,curses.COLOR_YELLOW,curses.COLOR_BLACK)
+    stdscr.addstr(0,0,"drokk\n", curses.A_REVERSE)
+    stdscr.refresh()
 
     with open("data.json","r") as data:
         timeline = json.load(data)
 
     for tweet in timeline:
-        header = "@" + tweet["user"]["screen_name"] + " (" + tweet["user"]["name"] + ") :\n"
-        content = "\t" + tweet["text"] + "\n"
-        stdscr.addstr(header)
+        handle = "@" + tweet["user"]["screen_name"] 
+        stdscr.addstr(handle,curses.color_pair(HANDLE_COLOR))
+        stdscr.addstr("\t")
+        name = tweet["user"]["name"] 
+        stdscr.addstr(" (" + name + ")\n", curses.color_pair(NAME_COLOR))
+        content = tweet["text"] + "\n"
         stdscr.addstr(content)
         stdscr.addstr("\n")
     
