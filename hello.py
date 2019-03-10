7a9afbb24795285424f0a324d24b7908e0eef5ec
dgm9704 <dgm9704@hotmail.com>
2019-03-10 23:11:56 +0200

    renaming things a bit

--- hello.py
+++ hello.py
@@ -8,8 +8,8 @@ def main(stdscr):
     stdscr.addstr("Hello World!!!")
     stdscr.refresh()
     stdscr.getch()
-    with open("content.json","r") as content:
-        json.load(content)
+    with open("content.json","r") as data:
+        content = json.load(data)
 
 if __name__ == '__main__':
     wrapper(main)
