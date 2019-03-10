3a824e968475170e766d93581eaa03898640bd7f
dgm9704 <dgm9704@hotmail.com>
2019-03-10 22:58:30 +0200

    (fix and ) read data

--- hello.py
+++ hello.py
@@ -1,12 +1,15 @@
  
 # hello.py
 
+import json
 from curses import wrapper
 
 def main(stdscr):
     stdscr.addstr("Hello World!!!")
     stdscr.refresh()
     stdscr.getch()
+    with open("content.json","r") as content:
+        json.load(content)
 
 if __name__ == '__main__':
     wrapper(main)
