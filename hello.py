be0f0e4199aded2f660ad5d5326c409a52391c4a
dgm9704 <dgm9704@hotmail.com>
2019-03-11 01:13:33 +0200

    some refactoring

--- hello.py
+++ hello.py
@@ -5,11 +5,13 @@ import json
 from curses import wrapper
 import curses
 
+HANDLE_COLOR = 1
+NAME_COLOR = 2
+
 def main(stdscr):
-    HANDLE_COLOR = 1
-    NAME_COLOR = 2
     curses.init_pair(HANDLE_COLOR,curses.COLOR_MAGENTA,curses.COLOR_BLACK)
     curses.init_pair(NAME_COLOR,curses.COLOR_YELLOW,curses.COLOR_BLACK)
+    
     stdscr.addstr(0,0,"drokk\n", curses.A_REVERSE)
     stdscr.refresh()
 
@@ -17,17 +19,21 @@ def main(stdscr):
         timeline = json.load(data)
 
     for tweet in timeline:
-        handle = "@" + tweet["user"]["screen_name"] 
-        stdscr.addstr(handle,curses.color_pair(HANDLE_COLOR))
-        stdscr.addstr("\t")
-        name = tweet["user"]["name"] 
-        stdscr.addstr(" (" + name + ")\n", curses.color_pair(NAME_COLOR))
-        content = tweet["text"] + "\n"
-        stdscr.addstr(content)
-        stdscr.addstr("\n")
-    
+        output_tweet(tweet,stdscr)
+       
     stdscr.refresh()
     stdscr.getch()
+
+def output_tweet(tweet,win):
+    handle = "@" + tweet["user"]["screen_name"] 
+    win.addstr(handle,curses.color_pair(HANDLE_COLOR))
+    win.addstr("\t")
+    name = tweet["user"]["name"] 
+    win.addstr(" (" + name + ")\n", curses.color_pair(NAME_COLOR))
+    content = tweet["text"] + "\n"
+    win.addstr(content)
+    win.addstr("\n")
+
 
 if __name__ == '__main__':
     wrapper(main)
