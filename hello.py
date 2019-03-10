a6427e4fb0686b887b871f158c3cf492c1cd378e
dgm9704 <dgm9704@hotmail.com>
2019-03-11 00:12:11 +0200

    some simple output straight to console

--- hello.py
+++ hello.py
@@ -4,12 +4,26 @@
 import json
 from curses import wrapper
 
-def main(stdscr):
-    stdscr.addstr("Hello World!!!")
-    stdscr.refresh()
-    stdscr.getch()
-    with open("content.json","r") as data:
+def main():# (stdscr):
+    #stdscr.addstr("Hello World!!!")
+    #stdscr.refresh()
+    #stdscr.getch()
+
+    with open("data.json","r") as data:
         content = json.load(data)
 
+    for tweet in content:
+        print("@" + tweet["user"]["screen_name"] + " (" + tweet["user"]["name"] + ") :")
+        print("\t" + tweet["text"])
+        #for k,v in tweet.items():
+        #    print(k,v)
+            #stdscr.addstr(str(v)) #str(k) + " -> " + str(v))
+        print("\n")
+        #stdscr.addstr("----------")
+    
+    #stdscr.refresh()
+    #stdscr.getch()
+
 if __name__ == '__main__':
-    wrapper(main)
+    main()
+    #wrapper(main)
